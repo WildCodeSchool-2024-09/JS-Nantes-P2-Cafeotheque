57c82f6e8c18e097ad982b84446ba617a2c89ef9
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-11-07 17:13:26 +0100

    made the general settings for the header.tsx file and connected it to the header.css file

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,1 +1,16 @@
-import ./Header.css
+import "./assets/style/Header.css";
+
+function Header() {
+  return (
+    <>
+      <header>
+        <nav>
+          <a href="">
+            <img alt="" />
+          </a>
+        </nav>
+      </header>
+    </>
+  );
+}
+export default Header;
