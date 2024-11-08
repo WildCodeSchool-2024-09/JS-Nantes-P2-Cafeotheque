539ad6c90554dde35b7224a86588a6bb4d6c9099
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-11-08 11:24:24 +0100

    attempted to fix some issues declared by Biome while committing

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,19 +1,31 @@
-import "./assets/style/Header.css";
+import "../assets/style/Header.css";
 
 function Header() {
   return (
     <>
-      <header>
-        <nav>
-          <a href="./pages/HomePage.tsx">
-            <img src="./images/logo-coffee.png" alt="Logo Coffee" />
-          </a>
-          <h1>Caféothèque</h1>
-        </nav>
-        <a href="./pages/ProfilePage.tsx">
-          <img src="./images/avatar.png" alt="Profile Avatar" />
-        </a>
-      </header>
+      <body>
+        <header>
+          <nav>
+            <a href="../src/pages/HomePage.tsx">
+              <img
+                className="logo-coffee"
+                src="../src/assets/images/logo-coffee.png"
+                alt="Logo Coffee"
+              />
+            </a>
+
+            <h1>Caféothèque</h1>
+
+            <a href="./pages/ProfilePage.tsx">
+              <img
+                className="avatar-profile"
+                src="../src/assets/images/avatar.png"
+                alt="Profile Avatar"
+              />
+            </a>
+          </nav>
+        </header>
+      </body>
     </>
   );
 }
