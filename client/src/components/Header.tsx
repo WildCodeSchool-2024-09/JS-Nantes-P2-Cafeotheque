b0d73f93e094a54eb57d0c17b7789bb8fa6dd7dc
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-11-08 12:04:00 +0100

    deleted the body balise from the component Header.tsx to avoid crash in the project

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,29 +3,27 @@ import "../assets/style/Header.css";
 function Header() {
   return (
     <>
-      <body>
-        <header>
-          <nav>
-            <a href="../src/pages/HomePage.tsx">
-              <img
-                className="logo-coffee"
-                src="../src/assets/images/logo-coffee.png"
-                alt="Logo Coffee"
-              />
-            </a>
+      <header>
+        <nav>
+          <a href="../src/pages/HomePage.tsx">
+            <img
+              className="logo-coffee"
+              src="../src/assets/images/logo-coffee.png"
+              alt="Logo Coffee"
+            />
+          </a>
 
-            <h1>Caféothèque</h1>
+          <h1>Caféothèque</h1>
 
-            <a href="./pages/ProfilePage.tsx">
-              <img
-                className="avatar-profile"
-                src="../src/assets/images/avatar.png"
-                alt="Profile Avatar"
-              />
-            </a>
-          </nav>
-        </header>
-      </body>
+          <a href="./pages/ProfilePage.tsx">
+            <img
+              className="avatar-profile"
+              src="../src/assets/images/avatar.png"
+              alt="Profile Avatar"
+            />
+          </a>
+        </nav>
+      </header>
     </>
   );
 }
