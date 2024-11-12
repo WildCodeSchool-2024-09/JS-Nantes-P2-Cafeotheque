c7496be0b8e0fcdc36c60909ad64e7a6c076ef6c
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2024-11-12 14:15:04 +0100

    🏗️: added and using globals.css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import "./App.css";
+import "./assets/globals.css";
 
 function App() {
   return (
