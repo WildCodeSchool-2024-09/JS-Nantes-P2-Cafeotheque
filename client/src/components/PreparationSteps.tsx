1afe894401fb325505b1c9137f25e650e7718621
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-11-13 17:17:07 +0100

    worked on the recipe page components

--- client/src/components/PreparationSteps.tsx
+++ client/src/components/PreparationSteps.tsx
@@ -1,19 +1,37 @@
 import "/src/pages/RecipeePage/PreparationSteps.css";
+
 function PreparationSteps() {
   return (
     <>
-      <main>
+      <article>
         <h1>Idée recette : le tiramisu</h1>
         <h2>Préparation</h2>
         <h3>Ėtape 1</h3>
+        <p>
+          Séparer les blancs des jaunes d'oeufs. Mélanger les jaunes avec le
+          sucre roux et le sucre vanillé.
+        </p>
         <h3>Ėtape 2</h3>
+        <p>Ajouter le mascarpone au fouet.</p>
         <h3>Ėtape 3</h3>
+        <p>
+          Monter les blancs en neige et les incorporer délicatement à la spatule
+          au mélange précédent. Réserver.
+        </p>
         <h3>Ėtape 4</h3>
+        <p>
+          Mouiller les biscuits dans le café rapidement avant d'en tapisser le
+          fond du plat. Recouvrir d'une couche de crème au mascarpone puis
+          répéter l'opération en alternant couche de biscuits et couche de crème
+          en terminant par cette dernière. Saupoudrer de cacao.
+        </p>
         <h3>Ėtape 5</h3>
+        <p>Mettre au réfrigérateur 4 heures minimum puis déguster frais.</p>
         <h2>La recette en vidéo</h2>
-      </main>
+      </article>
       ;
     </>
   );
 }
+
 export default PreparationSteps;
--- client/src/pages/RecipeePage/RecipeePage.tsx
+++ client/src/pages/RecipeePage/RecipeePage.tsx
@@ -1,22 +1,18 @@
-import "/src/pages/RecipeePage/RecipeePage.css";
 import IngredientsList from "../../components/IngredientsList";
+import PreparationSteps from "../../components/PreparationSteps";
+import "./RecipeePage.css";
 
-function RecipeePage() {
+const RecipeePage = () => {
   return (
-    <>
-      <IngredientsList />
-      <main>
-        <h1>Idée recette : le tiramisu</h1>
-        <h2>Préparation</h2>
-        <h3>Ėtape 1</h3>
-        <h3>Ėtape 2</h3>
-        <h3>Ėtape 3</h3>
-        <h3>Ėtape 4</h3>
-        <h3>Ėtape 5</h3>
-        <h2>La recette en vidéo</h2>
-      </main>
-    </>
+    <div className="recipee-page">
+      <div className="ingredients-list">
+        <IngredientsList />
+      </div>
+      <div className="preparation-steps">
+        <PreparationSteps />
+      </div>
+    </div>
   );
-}
+};
 
 export default RecipeePage;
