cb27a399f8e050d1293e1d75986bf301274d297e
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-11-13 12:28:32 +0100

    started the main section of the page which is the preparation steps of the recipe area

--- client/src/pages/RecipeePage/RecipeePage.tsx
+++ client/src/pages/RecipeePage/RecipeePage.tsx
@@ -3,26 +3,37 @@ import "/src/pages/RecipeePage/RecipeePage.css";
 function RecipeePage() {
   return (
     <>
-      <aside>
-        <section className="tiramisu-illustration">
-          <img
-            src="src/assets/images/tiramisu.jpg"
-            alt="Illustration Tiramisu"
-          />
-        </section>
-        <section className="ingredients-list">
-          <h2 className="ingredients-title">Ingrédients</h2>
-          <ul>
-            <li> 3 oeufs</li>
-            <li>100 g de sucre roux</li>
-            <li>1 sachet de sucre vanillé</li>
-            <li>250 g de mascarpone</li>
-            <li>50 cl de café noir</li>
-            <li>24 biscuits à la cuillère</li>
-            <li>30 g de cacao amer</li>
-          </ul>
-        </section>
-      </aside>
+      <body>
+        <aside>
+          <section className="tiramisu-illustration">
+            <img
+              src="src/assets/images/tiramisu.jpg"
+              alt="Illustration Tiramisu"
+            />
+          </section>
+          <section className="ingredients-list">
+            <h2 className="ingredients-title">Ingrédients</h2>
+            <ul>
+              <li> 3 oeufs</li>
+              <li>100 g de sucre roux</li>
+              <li>1 sachet de sucre vanillé</li>
+              <li>250 g de mascarpone</li>
+              <li>50 cl de café noir</li>
+              <li>24 biscuits à la cuillère</li>
+              <li>30 g de cacao amer</li>
+            </ul>
+          </section>
+        </aside>
+        <main>
+          <h1> Idée recette : le tiramisu </h1>
+          <h2> Préparation</h2>
+          <h3>Ėtape 1</h3>
+          <h3>Ėtape 2</h3>
+          <h3>Ėtape 3</h3>
+          <h3>Ėtape 4</h3>
+          <h3>Ėtape 5</h3>
+        </main>
+      </body>
     </>
   );
 }
