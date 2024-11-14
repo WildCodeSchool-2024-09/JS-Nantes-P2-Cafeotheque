8bbae5ef1bbea817abaa547a385d7166f6d17934
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2024-11-14 10:07:39 +0100

    ✨: User redirected when trying to access register/login pages while logged in

--- client/src/pages/RegisterPage/RegisterPage.tsx
+++ client/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,6 +1,6 @@
 import "./RegisterPage.css";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 
 interface FormValueInterface {
   username: string;
@@ -85,6 +85,7 @@ function RegisterPage() {
     }
   }
 
+  if (localStorage.getItem("connected-user")) return <Navigate to="/profile" />;
   return (
     <main id="register-container">
       <h2>Inscription</h2>
