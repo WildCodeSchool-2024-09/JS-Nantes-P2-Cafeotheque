67fc24fd2c34ff2ab7f4c2b893455e3fdf58b0de
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2024-11-14 18:21:39 +0100

    ✨: Create number of elements per page component

---
+++ client/src/components/NbrOfElementsPerPage.tsx
@@ -0,0 +1,40 @@
+import "../assets/style/NbrOfElementsPerPage.css";
+
+interface NbrOfElementsPerPageProps {
+  possibleNumbers: number[];
+  setNbrOfElementsPerPage: React.Dispatch<React.SetStateAction<number>>;
+}
+
+function NbrOfElementsPerPage({
+  possibleNumbers,
+  setNbrOfElementsPerPage,
+}: NbrOfElementsPerPageProps) {
+  function handleClickNumberOfElems(nbr: number) {
+    setNbrOfElementsPerPage(nbr);
+  }
+  return (
+    <div id="elements-per-page-container">
+      <p>Nombre d'elements par page</p>
+      <ul id="elements-per-page">
+        {/* Map the table given in props of choices of elements per page */}
+        {possibleNumbers.map((el) => {
+          return (
+            <li
+              key={el}
+              className="pointer"
+              onKeyDown={(e) => {
+                if (e.key === "Enter") () => handleClickNumberOfElems(el);
+              }}
+              onClick={() => {
+                handleClickNumberOfElems(el);
+              }}
+            >
+              {el}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+export default NbrOfElementsPerPage;
