c9146499f17a37d7cbc79487f0b36d3d4d315b25
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-11-14 16:48:49 +0100

    worked on the ingredients list and preparation steps components

--- client/src/components/PreparationSteps.tsx
+++ client/src/components/PreparationSteps.tsx
@@ -6,40 +6,43 @@ function PreparationSteps() {
       <h1 className="idee-recette-tiramisu-title">
         Idée recette : le tiramisu
       </h1>
-      <h2 className="preparation-title">Préparation</h2>
-      <ol className="preparation-steps-list">
-        <li>
-          <p>
-            Séparer les blancs des jaunes d'oeufs. Mélanger les jaunes avec le
-            sucre roux et le sucre vanillé.
-          </p>
-        </li>
-        <li>
-          <p>Ajouter le mascarpone au fouet.</p>
-        </li>
-        <li>
-          <p>
-            Monter les blancs en neige et les incorporer délicatement à la
-            spatule au mélange précédent. Réserver.
-          </p>
-        </li>
-        <li>
-          <p>
-            Mouiller les biscuits dans le café rapidement avant d'en tapisser le
-            fond du plat.{" "}
-          </p>
-          <p>
-            Recouvrir d'une couche de crème au mascarpone puis répéter
-            l'opération en alternant couche de biscuits et couche de crème en
-            terminant par cette dernière. Saupoudrer de cacao.
-          </p>
-        </li>
-        <li>
-          <p>Mettre au réfrigérateur 4 heures minimum puis déguster frais.</p>
-        </li>
-      </ol>
-
-      <h2>La recette en vidéo</h2>
+      <section className="text-and-video-tiramisu-sections">
+        <h2 className="preparation-title">Préparation</h2>
+        <ol className="preparation-steps-list">
+          <li className="number-of-step">
+            <p>
+              Séparer les blancs des jaunes d'oeufs.
+              <br />
+              Mélanger les jaunes avec le sucre roux et le sucre vanillé.
+            </p>
+          </li>
+          <li className="number-of-step">
+            <p>Ajouter le mascarpone au fouet.</p>
+          </li>
+          <li className="number-of-step">
+            <p>
+              Monter les blancs en neige et les incorporer délicatement à la
+              spatule <br /> au mélange précédent. Réserver.
+            </p>
+          </li>
+          <li className="number-of-step">
+            <p>
+              Mouiller les biscuits dans le café rapidement avant d'en tapisser
+              le fond du plat.
+            </p>
+            <p>
+              Recouvrir d'une couche de crème au mascarpone puis répéter
+              l'opération en <br /> alternant couche de biscuits et couche de
+              crème en terminant par cette dernière.
+              <br />
+              Saupoudrer de cacao.
+            </p>
+          </li>
+          <li className="number-of-step">
+            <p>Mettre au réfrigérateur 4 heures minimum puis déguster frais.</p>
+          </li>
+        </ol>
+      </section>
     </main>
   );
 }
