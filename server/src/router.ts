c9be217de6cd6c31cf14db9a1c59c9c69fd6452a
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2024-11-15 11:53:51 +0100

    ✨: Create route for recipe and byId for coffee and recipe

--- server/src/router.ts
+++ server/src/router.ts
@@ -13,10 +13,29 @@ router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
 
-import coffeeData from "./mocks/apiMock.json";
+import coffeeData from "./mocks/CoffeeMock.json";
+import RecipeData from "./mocks/RecipeeMock.json";
 
 router.get("/api/coffee", (_, res) => {
   res.status(200).json(coffeeData);
+});
+
+router.get("/api/coffee/:id", (req, res) => {
+  const id = req.params.id;
+  for (const el of coffeeData)
+    if (el.id === Number.parseInt(id)) res.status(200).json(el);
+  res.status(404).send("Coffee was not found");
+});
+
+router.get("/api/recipe", (_, res) => {
+  res.status(200).json(RecipeData);
+});
+
+router.get("/api/recipe/:id", (req, res) => {
+  const id = req.params.id;
+  for (const el of RecipeData)
+    if (el.id === Number.parseInt(id)) res.status(200).json(el);
+  res.status(404).send("Recipe was not found");
 });
 
 /* ************************************************************************* */
