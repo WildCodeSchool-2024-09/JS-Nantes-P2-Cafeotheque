203346adb46849ffb514804d718ce54b69cdace1
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-11-15 16:38:33 +0100

    work in progress

--- client/src/pages/RecipeePage/RecipeePage.tsx
+++ client/src/pages/RecipeePage/RecipeePage.tsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from "react";
 import IngredientsList from "../../components/IngredientsList";
 import PreparationSteps from "../../components/PreparationSteps";
 import "./RecipeePage.css";
 
 function RecipeePage() {
+  const [recipe, setRecipe] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/api/recipe")
+      .then((res) => res.json())
+      .then((recipes) => setRecipe(recipes));
+  }, []);
+
   return (
-    <>
+    recipe && (
       <main className="main-recipe-tiramisu-page">
-        <IngredientsList />
+        <IngredientsList
+          imgSrc={recipe[0].aside.illustration}
+          ingredients={recipe[0].aside.ingredients}
+        />
         <PreparationSteps />
       </main>
-    </>
+    )
   );
 }
 
