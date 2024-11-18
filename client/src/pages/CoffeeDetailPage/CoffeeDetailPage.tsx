7f7b7e5d4c7da9756edec41c345277ac5751c94e
MellerinCharly <mellerincharly.pro@gmail.com>
2024-11-18 17:12:07 +0100

    :white_check_mark: : Change the div to something more semantic. Change css to lighten it and put var.

--- client/src/pages/CoffeeDetailPage/CoffeeDetailPage.tsx
+++ client/src/pages/CoffeeDetailPage/CoffeeDetailPage.tsx
@@ -3,22 +3,23 @@ import "./CoffeeDetailPage.css";
 
 function CoffeeDetailPage() {
   const [isClicked, setIsClicked] = useState(false);
-  const handleClick = () => {
+
+  const handleClick = (): void => {
     setIsClicked(!isClicked);
   };
 
   return (
-    <div className="coffee-detail-page">
+    <main className="coffee-detail-page">
       {/* Conteneur de l'image du café */}
-      <div className="content-container">
-        <div className="image-container">
+      <section className="content-container">
+        <figure className="image-container">
           <img
             src="https://i.ibb.co/VmKTCwc/Vietnam-coffee-beans-VOA.jpg"
             alt="Café"
           />
-        </div>
-        <div className="text-container">
-          <section>
+        </figure>
+        <article className="text-container">
+          <header>
             <h2>
               Café du Vietnam
               <button
@@ -38,12 +39,14 @@ function CoffeeDetailPage() {
                 />
               </button>
               {/* Mini Card avec flèche retour */}
-              <div className="mini-card">
+              <aside className="mini-card">
                 <span className="back-arrow">←</span>
-              </div>
+              </aside>
             </h2>
-            <p>Profil : Sucré</p>
-            <br />
+          </header>
+          <p>Profil : Sucré</p>
+          <br />
+          <section>
             <h3>Histoire</h3>
             <p>
               Un café corsé et sucré, avec des notes de chocolat noir et de lait
@@ -53,7 +56,8 @@ function CoffeeDetailPage() {
               populaire au Vietnam, offrant une riche expérience de dégustation
               qui combine intensité et douceur pour une expérience unique.
             </p>
-            <br />
+          </section>
+          <section>
             <h3>Conseil de préparation</h3>
             <p>
               Préparez ce café avec une méthode de filtrage vietnamienne
@@ -64,97 +68,96 @@ function CoffeeDetailPage() {
             </p>
           </section>
           <section className="info-section">
-            <div className="info-item">
+            <article className="info-item">
               <span className="label">Continent:</span>
               <span className="value">Asie</span>
-            </div>
-            <div className="info-item">
+            </article>
+            <article className="info-item">
               <span className="label">Pays:</span>
               <span className="value">Vietnam</span>
-            </div>
-            <div className="info-item">
+            </article>
+            <article className="info-item">
               <span className="label">Profil:</span>
               <span className="value">Sucré</span>
-            </div>
-            <div className="info-item">
+            </article>
+            <article className="info-item">
               <span className="label">Prix:</span>
               <span className="value">€22.50</span>
-            </div>
+            </article>
           </section>
-        </div>
-      </div>
+        </article>
+      </section>
 
       {/* Section avec la carte du monde et les images */}
-      <div className="section-with-map-and-info">
-        <div className="map-container">
+      <section className="section-with-map-and-info">
+        <figure className="map-container">
           <img
             src="https://i.ibb.co/dMfxKms/Vietnam-orthographic-projection-svg.png"
             alt="Carte du monde"
             className="world-map"
-          />{" "}
+          />
           <img
             src="https://i.ibb.co/6DtJmbv/carte-vietnam-social.jpg"
             alt="Carte du Vietnam"
             className="vietnam-map-hover"
           />
-        </div>
-        <div className="extra-info">
-          <div className="coffee-grain">
+        </figure>
+        <section className="extra-info">
+          <figure className="coffee-grain">
             <img
               src="https://i.ibb.co/kh05r9N/pngegg-2.png"
               alt="Grains de café"
             />
-          </div>
-          <div className="card-container">
-            <div className="card">
-              <a href="/coffees-vietnam" className="card-link">
-                <h3>Lien vers tous les cafés de la même origine</h3>
-              </a>
-            </div>
-          </div>
-          <div className="coffee-leaves">
+          </figure>
+          <article className="card-container">
+            <a href="/coffees-vietnam" className="card-link">
+              <h3>Lien vers tous les cafés de la même origine</h3>
+            </a>
+          </article>
+          <figure className="coffee-leaves">
             <img
               src="https://i.ibb.co/Tw6jpvR/pngegg-2-2.png"
               alt="Feuilles de caféier"
             />
-          </div>
-        </div>
-      </div>
+          </figure>
+        </section>
+      </section>
 
       {/* Section des cartes avec des images de café des différentes régions (sous la carte du monde) */}
-      <div className="cards-container">
-        <div className="card-region">
+      <section className="cards-container">
+        <article className="card-region">
           <img
             src="https://i.ibb.co/rcSPZH2/000406-2023-1kg-perou-grains.jpg"
             alt="Café de Colombie"
           />
           <h3>Café de Colombie</h3>
-        </div>
+        </article>
 
-        <div className="card-region">
+        <article className="card-region">
           <img
             src="https://i.ibb.co/F8L7hHd/amerique-latine-fairtrade-bio.jpg"
             alt="Café du Brésil"
           />
           <h3>Café du Brésil</h3>
-        </div>
+        </article>
 
-        <div className="card-region">
+        <article className="card-region">
           <img
             src="https://i.ibb.co/wCJZqS7/grani-misclea-intensa-classica-01.jpg"
             alt="Café d'Éthiopie"
           />
           <h3>Café d'Éthiopie</h3>
-        </div>
-        <div className="card-region">
+        </article>
+
+        <article className="card-region">
           <img
             src="https://i.ibb.co/sbcPc99/cafe-bio-en-grains-origine-perou-max-havelaar-torrefacteur-sati.png"
             alt="Café du Costa Rica"
           />
           <h3>Café du Costa Rica</h3>
-        </div>
-      </div>
-    </div>
+        </article>
+      </section>
+    </main>
   );
 }
 
