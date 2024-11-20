f20a1f8da9a702475c5b65e5f6ba796a330c0012
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2024-11-20 15:50:17 +0100

    🩹: fix order biome

--- client/src/utils/context/AuthContext.tsx
+++ client/src/utils/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 import type React from "react";
 import type { Dispatch, SetStateAction } from "react";
 import type { UserModel } from "../../models";
