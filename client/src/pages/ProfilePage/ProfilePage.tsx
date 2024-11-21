f4c1afc3933a53b04fd67f250dbdc6755d370feb
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2024-11-21 10:27:50 +0100

    ✨: filter liked items done

--- client/src/pages/ProfilePage/ProfilePage.tsx
+++ client/src/pages/ProfilePage/ProfilePage.tsx
@@ -3,21 +3,30 @@ import ListItems from "../../components/ListItems";
 import type { DataModel, UserModel } from "../../models/index";
 import "./ProfilePage.css";
 import { Navigate } from "react-router-dom";
+import FilteredList from "../../components/FilteredList";
+import type { FiltersStateType } from "../../types/FilteredList";
 
 function ProfilePage() {
-  const [data, setData] = useState<DataModel[]>([]);
+  const [fullData, setfullData] = useState<DataModel[]>([]);
+  const [currentData, setCurrentData] = useState<DataModel[]>([]);
+  const [filters, setFilters] = useState<FiltersStateType>({
+    continent: { isActive: false, filters: [] },
+    country: { isActive: false, filters: [] },
+    profile: { isActive: false, filters: [] },
+  });
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch("http://localhost:4000/api/coffee");
       const fullData = await response.json();
-      const filteredData = filterData(fullData);
-      setData(filteredData);
+      const filteredData = filterLikedData(fullData);
+      setfullData(filteredData);
+      setCurrentData(filteredData);
     };
     fetchData();
   }, []);
 
-  function filterData(fullData: DataModel[]) {
+  function filterLikedData(fullData: DataModel[]) {
     const filteredData: DataModel[] = [];
     const likedArrayString = localStorage.getItem(
       "super-secured-database-users",
@@ -34,12 +43,26 @@ function ProfilePage() {
     return filteredData;
   }
 
-  // Replace div filters with filters component
+  // On filter change
+  useEffect(() => {
+    let newData = fullData;
+    // Loop for each key of filters
+    for (const key of Object.keys(filters) as (keyof FiltersStateType)[]) {
+      // If the filter isActive we filter our data
+      if (filters[key].isActive && filters[key].filters.length)
+        newData = newData.filter((el) =>
+          filters[key].filters.includes(el[key]),
+        );
+    }
+    newData;
+    setCurrentData(newData);
+  }, [filters, fullData]);
+
   if (!localStorage.getItem("connected-user")) return <Navigate to="/login" />;
   return (
     <main id="profile-page-main-container">
-      <div id="filters" />
-      <ListItems data={data} />
+      <FilteredList filters={filters} setFilters={setFilters} data={fullData} />
+      <ListItems data={currentData} />
     </main>
   );
 }
