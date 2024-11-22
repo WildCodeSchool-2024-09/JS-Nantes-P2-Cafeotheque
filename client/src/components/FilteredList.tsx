ca0eafb04bf0b503ef6ac46f74bbebf5f5023443
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2024-11-22 14:47:44 +0100

    🩹: Fix use of onchange instead of onclick + key problem

--- client/src/components/FilteredList.tsx
+++ client/src/components/FilteredList.tsx
@@ -6,7 +6,7 @@ import type {
 } from "../types/FilteredList";
 
 function FilteredList({ filters, setFilters, data }: FilteredListProps) {
-  const handleClick = (el: React.MouseEvent<HTMLInputElement>) => {
+  const handleClick = (el: React.ChangeEvent<HTMLInputElement>) => {
     const target = el.target as HTMLInputElement;
     const id = target.id as keyof FiltersStateType;
     setFilters((prevValues) => ({
@@ -65,46 +65,40 @@ function FilteredList({ filters, setFilters, data }: FilteredListProps) {
       {/* List main filters */}
       {Object.keys(filters).map((filterName) => {
         return (
-          <>
-            <li key={filterName}>
-              <input
-                className="pointer"
-                onClick={handleClick}
-                onChange={() => {}}
-                type="checkbox"
-                id={filterName}
-                name={filterName}
-                checked={filters[filterName as keyof FiltersStateType].isActive}
-              />
-              <label className="pointer" htmlFor={filterName}>
-                {filterName}
-              </label>
-            </li>
+          <li key={`main-filter-${filterName}`}>
+            <input
+              className="pointer"
+              onChange={handleClick}
+              type="checkbox"
+              id={filterName}
+              name={filterName}
+              checked={filters[filterName as keyof FiltersStateType].isActive}
+            />
+            <label className="pointer" htmlFor={filterName}>
+              {filterName}
+            </label>
             {/* List sub-filters if main is checked */}
             {filters[filterName as keyof FiltersStateType].isActive && (
               <ul className="list-filter">
                 {getDifferentElement(filterName as keyof DataModel).map(
                   (el) => {
                     return (
-                      <li key={`${el}-porte`}>
+                      <li key={`sub-filter-${filterName}-${el}`}>
                         <input
                           className="pointer"
                           data-filtertype={filterName}
-                          key={el}
                           type="checkbox"
                           onClick={handleClickSub}
                           name={el}
                         />
-                        <label key={`${el}-label`} htmlFor={el}>
-                          {el}
-                        </label>
+                        <label htmlFor={el}>{el}</label>
                       </li>
                     );
                   },
                 )}
               </ul>
             )}
-          </>
+          </li>
         );
       })}
     </ul>
