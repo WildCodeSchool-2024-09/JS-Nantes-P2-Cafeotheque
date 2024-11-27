7ce89f403436c2b95963a4e0b1dfc375c2e32ed8
Naïma PANELLE <168522370+naiiipan44@users.noreply.github.com>
2024-11-27 17:27:08 +0100

    worked on the css of the recipe page and documented myself on way to refactor it and its css

--- client/src/pages/RecipePage/RecipePage.tsx
+++ client/src/pages/RecipePage/RecipePage.tsx
@@ -16,25 +16,26 @@ function RecipePage() {
 
   return (
     recipe && (
-      <body className="main-recipe-tiramisu-page">
-        <h1 className="idee-recette-tiramisu-title">
-          {`Idée recette : ${recipe[0].mainContent.recipeName}`}
-        </h1>
-        <section className="recipe-components">
-          <aside className="ingredients-list-section">
+      <body className="body-recipe-page">
+        <header className="recipe-page-secondary-header">
+          <h1 className="recipe-page-api-title">
+            {`Idée recette : ${recipe[0].mainContent.recipeName}`}
+          </h1>
+        </header>
+        <main className="main-recipe-page-card">
+          <aside className="ingredients-list-card">
             <IngredientsList
               imgSrc={recipe[0].aside.illustration}
               ingredients={recipe[0].aside.ingredients}
             />
           </aside>
-          <section className="prep-steps-and-videp-section">
+          <article className="prep-steps-and-video-card">
             <PreparationSteps
               preparationSteps={recipe[0].mainContent.preparationSteps}
             />
-
             <RecipeVideo videoUrl={recipe[0].mainContent.videoUrl} />
-          </section>
-        </section>
+          </article>
+        </main>
       </body>
     )
   );
