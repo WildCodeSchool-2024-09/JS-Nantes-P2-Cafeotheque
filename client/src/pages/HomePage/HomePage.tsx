162f406f1041b67b5bd0087c1dc94118cf07183b
FarCodeFR <Timo-44@hotmail.com>
2024-11-28 16:58:00 +0100

    Add margin-bottom for main

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,15 @@
 import HomeCards from "../../components/HomeCards";
 import HomeNav from "../../components/HomeNav";
 import LatestArrival from "../../components/LatestArrival";
+import "./homepage.css";
 
 function HomePage() {
   return (
-    <>
+    <main>
       <HomeNav />
       <LatestArrival />
       <HomeCards />
-    </>
+    </main>
   );
 }
 
